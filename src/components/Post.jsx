d027e874045b4a920c46658655d88454f90fdb15
UmitKok21 <umit.kok21@gmail.com>
2025-03-20 12:47:41 +0300

    Implemented random name selection in Post component.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,9 @@
+const names = ['Maximillian', 'Manuel'];
 function Post() {
+    const choosenName = Math.random() > 0.5 ? names[0] : names[1];
     return (
         <div>
-            <p>Maximillian</p>
+            <p>{choosenName}</p>
             <p>React js is awesome!</p>
         </div>
     );

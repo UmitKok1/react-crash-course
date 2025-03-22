deb412943f2c2d0f6bf66c6f589cf4290e133ddf
UmitKok21 <umit.kok21@gmail.com>
2025-03-22 20:15:58 +0300

    Implement Modal component with backdrop and dialog structure.

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from './PostsList.module.css';
+import Modal from './Modal';
+
 function PostsList() {
     const [enteredBody, setEnteredBody] = useState('');
     const [enteredAuthor, setEnteredAuthor] = useState('');
@@ -15,7 +17,13 @@ function PostsList() {
     }
     return (
         <>
-            <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+            <Modal>
+                <NewPost
+                    onBodyChange={bodyChangeHandler}
+                    onAuthorChange={authorChangeHandler}
+                />
+            </Modal>
+
             <ul className={classes.posts}>
                 <Post author={enteredAuthor} body={enteredBody} />
                 <Post author="Manuel" body="Check out the full course!" />

6e9b7116add5f383ee725372e5b567bb3dbc571c
UmitKok21 <umit.kok21@gmail.com>
2025-03-22 20:37:02 +0300

    Add modal functionality to PostsList component with dynamic state handling.

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -5,8 +5,13 @@ import classes from './PostsList.module.css';
 import Modal from './Modal';
 
 function PostsList() {
+    const [modalIsVisible, setModalIsVisible] = useState(true);
     const [enteredBody, setEnteredBody] = useState('');
     const [enteredAuthor, setEnteredAuthor] = useState('');
+
+    function hideModalHandler() {
+        setModalIsVisible(false);
+    }
 
     function bodyChangeHandler(event) {
         setEnteredBody(event.target.value);
@@ -15,20 +20,24 @@ function PostsList() {
     function authorChangeHandler(event) {
         setEnteredAuthor(event.target.value);
     }
+
     return (
         <>
-            <Modal>
-                <NewPost
-                    onBodyChange={bodyChangeHandler}
-                    onAuthorChange={authorChangeHandler}
-                />
-            </Modal>
+            {modalIsVisible && (
+                <Modal onClose={hideModalHandler}>
+                    <NewPost
+                        onBodyChange={bodyChangeHandler}
+                        onAuthorChange={authorChangeHandler}
+                    />
+                </Modal>
+            )}
 
             <ul className={classes.posts}>
                 <Post author={enteredAuthor} body={enteredBody} />
                 <Post author="Manuel" body="Check out the full course!" />
             </ul>
         </>
-    )
+    );
 }
+
 export default PostsList;
